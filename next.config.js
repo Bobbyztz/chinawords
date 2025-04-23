1225d3181bc5a3374e09ca61605036545ebd798d
zhangtianzong <legendztz@outlook.com>
2025-04-22 19:21:50 -0500

    install MDX

--- next.config.js
+++ next.config.js
@@ -1,5 +1,17 @@
 /** @type {import('next').NextConfig} */
+const withMDX = require('@next/mdx')({
+  extension: /\.mdx?$/,
+  options: {
+    remarkPlugins: [],
+    rehypePlugins: [],
+    // If you use `MDXProvider`, uncomment the following line.
+    providerImportSource: "@mdx-js/react",
+  },
+});
+
 const nextConfig = {
+  // Configure pageExtensions to include md and mdx
+  pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx'],
   images: {
     domains: ['images.unsplash.com'],
     remotePatterns: [
@@ -12,4 +24,5 @@ const nextConfig = {
   },
 };
 
-module.exports = nextConfig;
+// Merge MDX config with Next.js config
+module.exports = withMDX(nextConfig);
