2c4b42b8c9f556895805fb894da6b2510add6683
zhangtianzong <legendztz@outlook.com>
2025-04-27 11:08:24 -0500

    update 基础板块进阶板块 ui

--- app/newHomePage/page.tsx
+++ app/newHomePage/page.tsx
@@ -75,7 +75,7 @@ export default function NewHomePage() {
         >
           <div className="bg-white/30 backdrop-blur-sm rounded-lg shadow-lg w-full h-[calc(100%-9px)] overflow-y-auto">
             <div className="h-full p-6">
-              <h2 className="text-3xl font-bold text-center mb-8 font-serif-sc">
+              <h2 className="text-3xl font-bold text-center pt-10 mb-16 font-serif-sc">
                 基础板块
               </h2>
               <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6">
@@ -135,7 +135,7 @@ export default function NewHomePage() {
         >
           <div className="bg-white/30 backdrop-blur-sm rounded-lg shadow-lg w-full h-[calc(100%-9px)] overflow-y-auto">
             <div className="h-full p-6">
-              <h2 className="text-3xl font-bold text-center mb-8 font-serif-sc">
+              <h2 className="text-3xl font-bold text-center pt-10 mb-16 font-serif-sc">
                 进阶板块
               </h2>
               <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6">
