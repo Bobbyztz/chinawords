3fed249c4f4f55545d8a912990aa44cb9d4dd2d6
zhangtianzong <legendztz@outlook.com>
2025-06-11 22:16:07 -0500

    style: adjust top margin spacing in FoodImageGrid from mt-6 to mt-2

--- app/food/components/FoodImageGrid.tsx
+++ app/food/components/FoodImageGrid.tsx
@@ -43,7 +43,7 @@ const FoodImageGrid: React.FC<FoodImageGridProps> = ({
 
   return (
     <div className="space-y-8 pb-8 h-full">
-      <div className="mt-6">
+      <div className="mt-2">
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 xl:grid-cols-4 gap-4">
           {images.map((image, index) => (
             <FoodImageCard
